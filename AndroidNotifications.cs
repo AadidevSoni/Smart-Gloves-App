93f3f6e58d168235be8e47598a0189a44e5f70bc
AadidevSoni <aadidevbahrain@gmail.com>
2025-04-05 13:32:11 +0530

    android-notification-popup

--- AndroidNotifications.cs
+++ AndroidNotifications.cs
@@ -1,47 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Unity.Notifications.Android;
-using UnityEngine.Android;
+using UnityEngine;
 
 public class AndroidNotifications : MonoBehaviour
 {
-    //Request authorization
-    public void RequestAuthorization(){
-        if(!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
+    public string channelId = "wearer_alerts";
+
+    public void RequestAuthorization()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.Notification))
         {
-            Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
+            Permission.RequestUserPermission(Permission.Notification);
         }
     }
 
-    //Register a channel
     public void RegisterNotificationChannel()
     {
-        var channel = new AndroidNotificationChannel
+        var channel = new AndroidNotificationChannel()
         {
-            Id = "default_channel",
-            Name = "Default Channel",
-            Importance = Importance.Default,
-            Description = "Full Lives"
+            Id = channelId,
+            Name = "Wearer Alert Notifications",
+            Importance = Importance.High,  //for pop up
+            Description = "Alerts for needs like water, food, help, etc.",
+            EnableVibration = true,
+            EnableLights = true,
+            CanBypassDnd = true,
+            LockScreenVisibility = LockScreenVisibility.Public
         };
 
         AndroidNotificationCenter.RegisterNotificationChannel(channel);
     }
 
-    //set up notification template
-
-    public void SendNotification(string title, string text, int fireTimeinSeconds)
+    public void SendNotification(string title, string message, int delaySeconds)
     {
-        var notification = new AndroidNotification();
-        notification.Title = title;
-        notification.Text = text;
-        notification.FireTime = System.DateTime.Now.AddSeconds(fireTimeinSeconds);
-        notification.SmallIcon = "icon_0";
-        notification.LargeIcon = "icon_1";
+        var notification = new AndroidNotification()
+        {
+            Title = title,
+            Text = message,
+            FireTime = System.DateTime.Now.AddSeconds(delaySeconds),
+            ShouldAutoCancel = true,
+            SmallIcon = "icon_0", // Optional: Add custom icon in Plugins/Android/res
+            LargeIcon = "icon_0"
+        };
 
-        AndroidNotificationCenter.SendNotification(notification, "default_channel");
+        AndroidNotificationCenter.SendNotification(notification, channelId);
     }
-
-
-
 }
